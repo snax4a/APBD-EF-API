8563b4b5be75180bbcd5215f390850dc535a3d73
Szymon Sus <sus.szymon3@gmail.com>
2020-06-05 12:53:33 +0200

    Added Doctors CRUD API methods

--- WebApplication1/Controllers/DoctorsController.cs
+++ WebApplication1/Controllers/DoctorsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs.requests;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -20,7 +21,77 @@ namespace WebApplication1.Controllers
 
         public IActionResult GetDoctors()
         {
-            return Ok(_context.Doctors.ToList());
+            var doctors =  _context.Doctors.Select(d => new Doctor
+            {
+                IdDoctor = d.IdDoctor,
+                FirstName = d.FirstName,
+                LastName = d.LastName,
+                Email = d.Email
+            }).ToList();
+
+            return Ok(doctors);
+        }
+
+        [HttpPost]
+        public IActionResult AddDoctor(AddDoctorRequest request)
+        {
+            Doctor doctor = new Doctor
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            };
+
+            _context.Add(doctor);
+            _context.SaveChanges();
+
+            string uri = $"/api/doctors/{doctor.IdDoctor}";
+
+            return Created(uri,doctor);
+        }
+
+        [HttpPut("{idDoctor}")]
+        public IActionResult UpdateDoctor(UpdateDoctorRequest request, int idDoctor)
+        {
+            var doctor = new Doctor { IdDoctor = idDoctor };
+
+            _context.Attach(doctor);
+
+            if (request.FirstName != null)
+            {
+                doctor.FirstName = request.FirstName;
+                _context.Entry(doctor).Property("FirstName").IsModified = true;
+            }
+
+            if (request.LastName != null)
+            {
+                doctor.LastName = request.LastName;
+                _context.Entry(doctor).Property("LastName").IsModified = true;
+            }
+
+            if (request.Email != null)
+            {
+                doctor.Email = request.Email;
+                _context.Entry(doctor).Property("Email").IsModified = true;
+            }
+
+            _context.SaveChanges();
+
+            string uri = $"/api/doctors/{doctor.IdDoctor}";
+
+            return Created(uri, doctor);
+        }
+
+        [HttpDelete("{idDoctor}")]
+        public IActionResult DeleteDoctor(int idDoctor)
+        {
+            var doctor = new Doctor { IdDoctor = idDoctor };
+
+            _context.Attach(doctor);
+            _context.Remove(doctor);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
